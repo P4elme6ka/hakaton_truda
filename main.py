c26985e45fe3f8435fb6996c6c96f70373e79fa4
P4elme6ka <006gta1@gmail.com>
2020-07-12 11:21:05 +0300

    delete sheet

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from flask import Flask, request
 from site_parcer import parce
 
-
 app = Flask(__name__)
+
 
 @app.route('/get/', methods=["POST"])
 def get_proffecion_words():
     url = request.args['url']
-    print(url)
-    return parce(url)
-
-
-@app.route('/get/<url>/')
-def get_proffecion(url):
     print(url)
     return parce(url)
 
